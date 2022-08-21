c9d55893b94b3b009d92c482b51198812f6e7a5f
Dmitry Boyarsky <boyarsky.ds@gmail.com>
2022-08-21 15:57:07 +0700

    Even.java added

---
+++ src/main/java/com/dmboyarsky/games/Even.java
@@ -0,0 +1,21 @@
+package com.dmboyarsky.games;
+
+import com.dmboyarsky.Engine;
+import com.dmboyarsky.Utils;
+
+public class Even {
+
+    private static final String CONDITION_GAME = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
+    public static void run() {
+        String[] questions = new String[Engine.ROUNDS];
+        String[] answers = new String[Engine.ROUNDS];
+        for (int i = 0; i < Engine.ROUNDS; i++) {
+            int number = Utils.getRandomNumber(Utils.MIN, Utils.MAX);
+            questions[i] = String.valueOf(number);
+            answers[i] = number % 2 == 0 ? "yes" : "no";
+        }
+        Engine.run(CONDITION_GAME, new String[][] {questions, answers});
+    }
+
+}
