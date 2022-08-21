e326afc5e19d10cc31e7afed43e76069c63e4614
Dmitry Boyarsky <boyarsky.ds@gmail.com>
2022-08-21 13:05:46 +0700

    Utils.java added

---
+++ src/main/java/com/dmboyarsky/Utils.java
@@ -0,0 +1,13 @@
+package com.dmboyarsky;
+
+import java.util.Random;
+
+public class Utils {
+    public static final int MAX = 100;
+    public static final int MIN = -50;
+
+    public static int getRandomNumber(int inclusiveMin, int inclusiveMax) {
+        Random random = new Random();
+        return inclusiveMin + random.nextInt(inclusiveMax + 1);
+    }
+}
