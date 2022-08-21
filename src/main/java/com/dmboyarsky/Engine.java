7c7e3ee1714648aa5f80a69f4365794eb61c4885
Dmitry Boyarsky <boyarsky.ds@gmail.com>
2022-08-21 15:44:50 +0700

    Engine.java modified

--- src/main/java/com/dmboyarsky/Engine.java
+++ src/main/java/com/dmboyarsky/Engine.java
@@ -3,4 +3,35 @@ package com.dmboyarsky;
 import java.util.Scanner;
 
 public class Engine {
+
+    public static final int ROUNDS = 3;
+    private static final int LINE_OF_QUESTIONS = 0;
+    private static final int LINE_OF_ANSWERS = 1;
+
+    public static void run(String rules, String[][] questionsAndAnswers) {
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?\n");
+        Scanner scanner = new Scanner(System.in);
+        String playerName = scanner.nextLine();
+        System.out.println("\nHello, " + playerName + "!\n");
+        System.out.println(rules + "\n");
+        for (int i = 0; i < ROUNDS; i++) {
+            String question = questionsAndAnswers[LINE_OF_QUESTIONS][i];
+            String answer = questionsAndAnswers[LINE_OF_ANSWERS][i];
+            System.out.println("Question: " + question);
+            String playerAnswer = getAnswer();
+            if (!answer.equals(playerAnswer)) {
+                System.out.println("\n" + "'" + playerAnswer + "' is wrong answer. Correct answer was '"
+                        + answer + "'.\nLet's try again, " + playerName + "!");
+                return;
+            }
+            System.out.println("Correct!\n");
+        }
+        System.out.println("Congratulations, " + playerName + "!");
+    }
+
+    private static String getAnswer() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().toLowerCase();
+    }
+
 }
